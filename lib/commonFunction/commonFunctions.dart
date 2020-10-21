17e832e3793d58ffdb48472d4a45b88dc6b5b7b7
Samyush <samyushmaharjan@gmail.com>
2020-10-21 20:15:02 +0545

    corrected working version

--- lib/commonFunction/commonFunctions.dart
+++ lib/commonFunction/commonFunctions.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class CommonFunction {
   CommonFunction({
     this.commonString,
+    // this.contexter,
+    this.onPressed,
   });
 
   final String commonString;
+  // final State contexter;
+  final Function onPressed;
 
   static Future<void> sendAMail(String url) async {
     if (await canLaunch(url)) {
@@ -35,5 +40,38 @@ class CommonFunction {
     } else {
       throw 'Could not launch $url';
     }
+  }
+
+//  the under function is for alert box
+  Future<void> showMyDialog() async {
+    return showDialog<void>(
+      // context: contexter,
+      // context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('SCHOOL APPS'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                    'For this to run you need Whats app pre-installed on your device.'),
+                Text('Would you like to proceed?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: onPressed,
+            ),
+            FlatButton(
+              child: Text('Proceed'),
+              onPressed: onPressed,
+            ),
+          ],
+        );
+      },
+    );
   }
 }
