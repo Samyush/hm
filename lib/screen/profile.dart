802268d5e3b2765159870e299636496727d53320
Samyush <samyushmaharjan@gmail.com>
2020-11-04 21:29:37 +0545

    corrected working version

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'file:///C:/FlutterApps/workFrames/hm/lib/commonFunction/constants.dart';
 
@@ -20,115 +21,97 @@ class _ProfileState extends State<Profile> {
       ),
       body: SafeArea(
         child: ListView(
-            // children: <Widget>[
-            //   Column(
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: <Widget>[
-            //       CircleAvatar(
-            //         radius: 80,
-            //         backgroundImage: AssetImage('images/4.png'),
-            //       ),
-            //       Text(
-            //         'Academy of Culinary Arts & Hospitality Management',
-            //         textAlign: TextAlign.center,
-            //         style: TextStyle(
-            //           fontFamily: 'Acme',
-            //           fontSize: 30,
-            //           color: Colors.red,
-            //           fontWeight: FontWeight.bold,
-            //           letterSpacing: 3,
-            //         ),
-            //       ),
-            //       Text(
-            //         'Lagankhel, Lalitpur',
-            //         style: TextStyle(
-            //           fontFamily: 'Acme',
-            //           fontSize: 20,
-            //           color: Colors.white,
-            //           fontWeight: FontWeight.bold,
-            //           letterSpacing: 3,
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         height: 20,
-            //         width: 250,
-            //         child: Divider(
-            //           color: Colors.white,
-            //         ),
-            //       ),
-            //       Card(
-            //         color: Color(0xFFB4B356),
-            //         margin: EdgeInsets.symmetric(
-            //           vertical: 10,
-            //           horizontal: 25,
-            //         ),
-            //         child: Padding(
-            //           padding: EdgeInsets.all(4),
-            //           child: ListTile(
-            //             leading: Icon(
-            //               Icons.perm_identity,
-            //               color: Colors.white,
-            //             ),
-            //             title: Text(
-            //               'Student ID:  S001',
-            //               style: TextStyle(
-            //                 fontSize: 20,
-            //                 color: Colors.white,
-            //                 fontFamily: 'Acme',
-            //                 letterSpacing: 3,
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //       Card(
-            //         color: Color(0xFFB4B356),
-            //         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            //         child: Padding(
-            //           padding: EdgeInsets.all(4),
-            //           child: ListTile(
-            //             leading: Icon(
-            //               FontAwesomeIcons.child,
-            //               color: Colors.white,
-            //             ),
-            //             title: Text(
-            //               'Name: Tom Cruise',
-            //               style: TextStyle(
-            //                 fontSize: 20,
-            //                 color: Colors.white,
-            //                 fontFamily: 'Acme',
-            //                 letterSpacing: 3,
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //       Card(
-            //         color: Color(0xFFB4B356),
-            //         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            //         child: Padding(
-            //           padding: EdgeInsets.all(4),
-            //           child: ListTile(
-            //             leading: Icon(
-            //               FontAwesomeIcons.voteYea,
-            //               color: Colors.white,
-            //             ),
-            //             title: Text(
-            //               'Year: 1st',
-            //               style: TextStyle(
-            //                 fontSize: 20,
-            //                 color: Colors.white,
-            //                 fontFamily: 'Acme',
-            //                 letterSpacing: 3,
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ],
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('images/4.png'),
+                ),
+                Text(
+                  'Academy of Culinary Arts & Hospitality Management',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Acme',
+                    fontSize: 30,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3,
+                  ),
+                ),
+                Text(
+                  'Lagankhel, Lalitpur',
+                  style: TextStyle(
+                    fontFamily: 'Acme',
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 250,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                NameCard(
+                  label: 'Student ID:  S001',
+                  icons: Icon(
+                    Icons.perm_identity,
+                    color: Colors.white,
+                  ),
+                ),
+                NameCard(
+                  label: 'Name: Tom Cruise',
+                  icons: Icon(
+                    FontAwesomeIcons.child,
+                    color: Colors.white,
+                  ),
+                ),
+                NameCard(
+                  label: 'Year: 1st',
+                  icons: Icon(
+                    FontAwesomeIcons.voteYea,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NameCard extends StatelessWidget {
+  NameCard({this.label, this.icons});
+
+  final String label;
+  final Icon icons;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0xFFB4B356),
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+      child: Padding(
+        padding: EdgeInsets.all(4),
+        child: ListTile(
+          leading: icons,
+          title: Text(
+            '$label',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontFamily: 'Acme',
+              letterSpacing: 3,
+            ),
+          ),
+        ),
       ),
     );
   }
