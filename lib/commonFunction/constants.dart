837980f7f2d40b68b4bcc89b965d4376e2b5c1ef
Samyush <samyushmaharjan@gmail.com>
2021-03-18 10:32:17 +0545

    ip address connection detailing. as per ipconfig or ifconfig

--- lib/commonFunction/constants.dart
+++ lib/commonFunction/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//the link to be used is LAN adapters WiFi -> IPv4 Address
 const kApiCurrentUsr = 'http://192.168.31.46:8000/api/currentUsr';
 const kApiMyRating = 'http://192.168.31.46:8000/api/rateMe';
 // 10.0.2.2
