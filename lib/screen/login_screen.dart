3cbda8fcfef24a3b3d0f6da317acde67de2e7a92
samyush <samyushmaharjan@gmail.com>
2021-04-03 08:06:27 +0545

    internet network check-success

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hm/componentBuilders/rounded_button.dart';
@@ -22,10 +24,62 @@ class _LoginScreenState extends State<LoginScreen> {
   String email;
   String password;
   bool showSpinner = false;
+  bool _connectionStatusNegative = true;
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
   //
+
+  //for wifi checker
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    ConnectivityResult result = ConnectivityResult.none;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    switch (result) {
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.mobile:
+        // case ConnectivityResult.none:
+        setState(() => _connectionStatusNegative = false);
+        break;
+      default:
+        setState(() => _connectionStatusNegative = true);
+        break;
+    }
+  }
+
   _showMyDialog() async {
     return showDialog<void>(
       context: context,
@@ -97,81 +151,87 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ModalProgressHUD(
-        inAsyncCall: showSpinner,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Flexible(
-                child: Hero(
-                    tag: 'logo',
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 200.0,
-                      child: Image.asset('images/clogo.png'),
-                    )),
+      body: _connectionStatusNegative
+          ? Center(
+              child: Text(
+              'Check Network Status',
+              style: TextStyle(color: Colors.red, fontSize: 20),
+            ))
+          : ModalProgressHUD(
+              inAsyncCall: showSpinner,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Flexible(
+                      child: Hero(
+                          tag: 'logo',
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 200.0,
+                            child: Image.asset('images/clogo.png'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 48.0,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      textAlign: TextAlign.center,
+                      controller: emailController,
+                      onChanged: (value) {
+                        email = value;
+                      },
+                      decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter Your Email'),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      controller: passwordController,
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter Your Password'),
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    RoundedButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            showSpinner = true;
+                          },
+                        );
+                        signIn(emailController.text, passwordController.text);
+                        // Navigator.pushNamed(context, BottomNavigationss.id);
+                      },
+                      // onPressed: emailController.text == "" ||
+                      //         passwordController.text == ""
+                      //     ? _showMyDialog
+                      //     : () {
+                      //         setState(
+                      //           () {
+                      //             showSpinner = true;
+                      //           },
+                      //         );
+                      //         signIn(emailController.text, passwordController.text);
+                      //         // Navigator.pushNamed(context, BottomNavigationss.id);
+                      //       },
+                      title: 'Log In',
+                      color: Colors.blueAccent,
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                controller: emailController,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration:
-                    kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                controller: passwordController,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter Your Password'),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                onPressed: () {
-                  setState(
-                    () {
-                      showSpinner = true;
-                    },
-                  );
-                  signIn(emailController.text, passwordController.text);
-                  // Navigator.pushNamed(context, BottomNavigationss.id);
-                },
-                // onPressed: emailController.text == "" ||
-                //         passwordController.text == ""
-                //     ? _showMyDialog
-                //     : () {
-                //         setState(
-                //           () {
-                //             showSpinner = true;
-                //           },
-                //         );
-                //         signIn(emailController.text, passwordController.text);
-                //         // Navigator.pushNamed(context, BottomNavigationss.id);
-                //       },
-                title: 'Log In',
-                color: Colors.blueAccent,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- lib/screen/rateMe.dart
+++ lib/screen/rateMe.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hm/componentBuilders/reuseCardBuilder.dart';
 import 'package:hm/componentBuilders/roundedIconButton.dart';
@@ -24,11 +28,57 @@ class _RateMeState extends State<RateMe> {
   int collegeInfrastructure = 10;
   int educationQuality = 10;
 
+  bool _connectionStatus = true;
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   @override
   void initState() {
     super.initState();
     getUserData();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     // rascal();
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    ConnectivityResult result = ConnectivityResult.none;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    switch (result) {
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.mobile:
+        // case ConnectivityResult.none:
+        setState(() => _connectionStatus = true);
+        break;
+      default:
+        setState(() => _connectionStatus = false);
+        break;
+    }
   }
 
   void getUserData() async {
@@ -94,7 +144,14 @@ class _RateMeState extends State<RateMe> {
     return Scaffold(
       appBar: AppBar(
         leading: kLeadingNav,
-        title: kAppbarTitle,
+        title: _connectionStatus
+            ? kAppbarTitle
+            : Center(
+                child: Text(
+                  'Check Network Status',
+                  style: TextStyle(color: Colors.red, fontSize: 20),
+                ),
+              ),
         actions: <Widget>[
           IconButton(
             icon: Icon(
