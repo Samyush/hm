3b0ca57879c436fcee6c85ff62e2cf39f16de685
Samyush <samyushmaharjan@gmail.com>
2021-04-12 16:06:12 +0545

    deployed URL

--- lib/networking/allAPI/allAPI.dart
+++ lib/networking/allAPI/allAPI.dart
@@ -1,7 +1,10 @@
 //the link to be used is LAN adapters WiFi -> IPv4 Address
 
-const KServerURL = '192.168.31.155';
-const kApiCurrentUsr = 'http://$KServerURL:8000/api/currentUsr';
-const kApiMyRating = 'http://$KServerURL:8000/api/rateMe';
-// 10.0.2.2
-final String kApiLogin = "http://$KServerURL:8000/api/login";
+const KServerURL = 'dry-gorge-89141.herokuapp.com';
+const kApiCurrentUsr = 'http://$KServerURL/api/currentUsr';
+const kApiMyRating = 'http://$KServerURL/api/rateMe';
+// 10.0.2.2:8000
+final String kApiLogin = "http://$KServerURL/api/login";
+
+// const kApiCurrentUsr = 'http://$KServerURL:8000/api/currentUsr';
+// http://dry-gorge-89141.herokuapp.com/api/register
